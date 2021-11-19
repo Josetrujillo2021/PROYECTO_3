19931479ff48b87162ee596be5d0bda8cc7c87c4
Josetrujillo2021 <tru19452@uvg.edu.gt>
2021-11-18 20:06:42 -0600

    NEOPIXEL
    Se agrego la programación para controlar el Neopixel

--- ESP_32/src/main.cpp
+++ ESP_32/src/main.cpp
@@ -1,9 +1,135 @@
-#include <Arduino.h>
+//Universidad Del Valle de Guatemala
+//BE3015: Electrónica Digital 2
+//José Trujillo
+//Proyecto #3
+// SPO2 y HR
+//----------------------------------------------------------------------------------------------------------------------
 
+//----------------------------------------------------------------------------------------------------------------------
+//Librerías
+//----------------------------------------------------------------------------------------------------------------------
+#include <Arduino.h>
+//librerías de NEOPIXEL
+#include <Adafruit_NeoPixel.h>
+
+
+//----------------------------------------------------------------------------------------------------------------------
+//Definición de pines
+//----------------------------------------------------------------------------------------------------------------------
+#define PIN 13
+//----------------------------------------------------------------------------------------------------------------------
+//Prototipos de funciones
+//----------------------------------------------------------------------------------------------------------------------
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(16, PIN, NEO_GRB + NEO_KHZ800);
+//---------------------------------------------------------------------------------------------------------------------
+//Variables Globales
+//----------------------------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------------------------
+//ISR  (interrupciones)
+//----------------------------------------------------------------------------------------------------------------------
+
+
+//----------------------------------------------------------------------------------------------------------------------
+//CONFIGURACIÓN
+//----------------------------------------------------------------------------------------------------------------------
 void setup() {
-  // put your setup code here, to run once:
+  
+  //Inicialización de NEOPIXEL
+  strip.begin();
+  strip.setBrightness(50);
+  strip.show(); // Initialize all pixels to 'off'
 }
 
+//---------------------------------------------------------------------------------------------------------------------
+//Loop principal
+//---------------------------------------------------------------------------------------------------------------------
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+}
+
+//Funciones para Neopixel
+// Secuencia de leds 1 encedido otro apagado de 1 en 1 de un solo color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+//Arcoiris
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Arcoiris girando en el neopixel
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 ciclos de todos los colores en la rueda
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+//recorrido de colores a traves de leds
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<10; j++) {  //10 ciclos de seguimiento
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, c);    //enciende cada 3 leds
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //apaga cada 3 leds
+      }
+    }
+  }
+}
+
+//recorrido de arcoiris a traves de los leds
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j=0; j < 256; j++) {     // ciclo de los 256 colores en toda la rueda
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //enciende cada 3 leds
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+// entrada de 0 a 255 para tener un color específico 
+//los colores van de orden R G B empezando desde R
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
